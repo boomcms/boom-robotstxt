e2b4405760f504947ca70652bbea7f2a2a8e9ec8
Rob Taylor <rob@uxblondon.com>
2014-11-17 23:40:16 +0000

    Added 'if not exists' to create table statement in migration

--- migrations/boom/20141114210100_install_robotstxt_editor.php
+++ migrations/boom/20141114210100_install_robotstxt_editor.php
@@ -6,7 +6,7 @@ class Migration_Boom_20141114210100 extends Minion_Migration_Base
 	public function up(Kohana_Database $db)
 	{
 		$db->query(null, "insert into roles (name, description) values ('manage_robots', 'Edit the robots.txt file')");
-		$db->query(null, "create table robots (id mediumint unsigned auto_increment primary key, rules text not null default '', edited_by mediumint unsigned, edited_at int(10) unsigned not null, foreign key (edited_by) references people(id) on update cascade on delete set null)");
+		$db->query(null, "create table if not exists robots (id mediumint unsigned auto_increment primary key, rules text not null default '', edited_by mediumint unsigned, edited_at int(10) unsigned not null, foreign key (edited_by) references people(id) on update cascade on delete set null)");
 	}
 
 	public function down(Kohana_Database $db)
