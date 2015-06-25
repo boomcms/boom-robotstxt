a5421db43a80a7dd3e28d4f9ba5619621441edaa
Rob Taylor <rob@uxblondon.com>
2015-06-25 18:07:23 +0100

    Fixed name of migration to install robots editor

--- src/database/migrations/2015_06_23_100000_install_robotstxt_editor.php
+++ src/database/migrations/2015_06_23_100000_install_robotstxt_editor.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 
 use Illuminate\Support\Facades\DB;
 
-class RobotstxtEditor extends Migration
+class InstallRobotstxtEditor extends Migration
 {
     /**
 	 * Run the migrations.
